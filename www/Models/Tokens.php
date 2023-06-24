8e68aca5775fbb25d6f351b92564286b46ee535e
skydogzz <thomas.stephan@live.fr>
2023-06-24 20:55:35 +0000

    create tokens class

---
+++ www/Models/Tokens.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+use App\Core\Sql;
+
+class Tokens extends Sql{
+
+    protected Int $id;
+    protected Int $user_id;
+    protected String $token;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param Int $id
+     */
+    public function setId(Int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return Int
+     */
+    public function getUserId(): Int
+    {
+        return $this->user_id;
+    }
+    
+    /**
+     * @param Int $user_id
+     */
+    public function setUserId(Int $user_id): void
+    {
+        $this->user_id = $user_id;
+    }
+
+    /**
+     * @return String
+     */
+    public function getToken(): String
+    {
+        return $this->token;
+    }
+
+    /**
+     * @param String $token
+     */
+    public function setToken(String $token): void
+    {
+        $this->token = $token;
+    }
+
+}

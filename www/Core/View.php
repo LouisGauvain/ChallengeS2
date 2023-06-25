a82c10429603889c14b380f6691ec144022e97f6
skydogzz <thomas.stephan@live.fr>
2023-06-25 02:21:17 +0000

    update session on each load

--- www/Core/View.php
+++ www/Core/View.php
@@ -1,29 +1,36 @@
 <?php
+
 namespace App\Core;
+
 use App\Core\Utils;
 use App\Models\Tokens;
-class View {
+
+class View
+{
 
     private String $view;
     private String $template;
     private $data = [];
 
-    public function __construct(String $view, String $template="back"){
+    public function __construct(String $view, String $template = "back")
+    {
         $this->setView($view);
 
         session_start();
 
-        if($template == "back" && isset($_SESSION['user'])){
+        if (isset($_SESSION['user'])) {
             $token = new Tokens();
             $token->setUserId($_SESSION['user']['id']);
             $token->setToken($_SESSION['token']);
-            if(!$token->checkToken()){
+            $token->updateToken();
+            if (!$token->checkToken()) {
                 session_destroy();
                 Utils::redirect("login");
             }
-            $token->updateToken();
-        } else
-        if($template == "back" && !isset($_SESSION['user'])){
+            Utils::setSessionToken($token->getToken());
+        }
+
+        if ($template == "back" && !isset($_SESSION['user'])) {
             Utils::redirect("login");
         }
 
@@ -33,34 +40,34 @@ class View {
 
     public function assign(String $key, $value): void
     {
-        $this->data[$key]=$value;
+        $this->data[$key] = $value;
     }
 
     public function setView(String $view): void
     {
-        $view = "Views/".trim($view).".view.php";
-        if(!file_exists($view)){
-            die("La vue ".$view." n'existe pas");
+        $view = "Views/" . trim($view) . ".view.php";
+        if (!file_exists($view)) {
+            die("La vue " . $view . " n'existe pas");
         }
         $this->view = $view;
     }
     public function setTemplate(String $template): void
     {
-        $template = "Views/".trim($template).".tpl.php";
-        if(!file_exists($template)){
-            die("Le template ".$template." n'existe pas");
+        $template = "Views/" . trim($template) . ".tpl.php";
+        if (!file_exists($template)) {
+            die("Le template " . $template . " n'existe pas");
         }
         $this->template = $template;
     }
 
-    public function modal($name, $config):void
+    public function modal($name, $config): void
     {
-        include "Views/Modals/".$name.".php";
+        include "Views/Modals/" . $name . ".php";
     }
 
-    public function __destruct(){
+    public function __destruct()
+    {
         extract($this->data);
         include $this->template;
     }
-
 }

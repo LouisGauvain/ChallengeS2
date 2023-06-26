2c1f50b762eb352dde661cc2705757f410b22708
louisgauvain <92375006+LouisGauvain@users.noreply.github.com>
2023-06-26 11:01:14 +0200

    add btn disconnect on page front and back tpl

--- www/Views/front.tpl.php
+++ www/Views/front.tpl.php
@@ -1,14 +1,20 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <title>Ma super page</title>
     <meta name="description" content="Ceci est ma super page">
 </head>
-<body>
-<h1>Template de front</h1>
 
-<?php include $this->view; ?>
+<body>
+    <h1>Template de front</h1>
+
+    <?php include $this->view; ?>
+
+    <a href="disconnect?controller=security&task=logout">Déconnexion</a>
+
 
 </body>
+
 </html>

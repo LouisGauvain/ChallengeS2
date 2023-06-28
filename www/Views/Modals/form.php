f87c0e79771cb9935998d5154a3709ba6fcbfa71
louisgauvain <92375006+LouisGauvain@users.noreply.github.com>
2023-06-28 16:49:22 +0200

    add label obligatoire on form

--- www/Views/Modals/form.php
+++ www/Views/Modals/form.php
@@ -1,26 +1,24 @@
-<form
-        method="<?= $config["config"]["method"]??"GET" ?>"
-        action="<?= $config["config"]["action"] ?>">
+<form method="<?= $config["config"]["method"] ?? "GET" ?>" action="<?= $config["config"]["action"] ?>">
 
-    <?php foreach ($config["inputs"] as $name=>$input):?>
+    <?php foreach ($config["inputs"] as $name => $input) : ?>
 
-        <?php if($input["type"] == "select"):?>
-            <select name="<?= $name;?>">
-                <?php foreach ($input["options"] as $option):?>
-                    <option><?= $option;?></option>
-                <?php endforeach;?>
-            </select>
-        <?php else: ?>
-            <input
-                    name="<?= $name;?>"
-                    type="<?= $input["type"]?>"
-                    placeholder=" <?= $input["placeholder"]?>"
-            >
-        <?php endif;?>
+        <?php if ($input["type"] == "select") : ?>
+            <label>
+                <?= $input["label"] ?>
+                <select name="<?= $name; ?>">
+                    <?php foreach ($input["options"] as $option) : ?>
+                        <option><?= $option; ?></option>
+                    <?php endforeach; ?>
+                </select>
+            </label>
+        <?php else : ?>
+            <label>
+                <?= $input["label"] ?>
+                <input name="<?= $name; ?>" type="<?= $input["type"] ?>" placeholder="<?= $input["placeholder"] ?>">
+            </label>
+        <?php endif; ?>
 
     <?php endforeach; ?>
-
-
 
     <input type="submit" name="submit" value="<?= $config["config"]["submit"] ?>">
     <input type="reset" value="<?= $config["config"]["cancel"] ?>">

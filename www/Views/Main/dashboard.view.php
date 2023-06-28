fc6d7bb04dcb28999e38a66087ae7c9fd1037275
skydogzz <thomas.stephan@live.fr>
2023-06-28 11:48:10 +0000

    add pagination if too much entries

--- www/Views/Main/dashboard.view.php
+++ www/Views/Main/dashboard.view.php
@@ -15,57 +15,70 @@ foreach ($user as $key => $value) {
 
 <?php
 if ($user['role_id'] == 1 && isset($users)) {
+    // Pagination configuration
+    $itemsPerPage = 1;
+    $currentPage = isset($_GET['page']) ? intval($_GET['page']) : 1;
+    $totalItems = count($users);
+    $totalPages = ceil($totalItems / $itemsPerPage);
+
+    // Calculate the index range for the current page
+    $startIndex = ($currentPage - 1) * $itemsPerPage;
+    $endIndex = $startIndex + $itemsPerPage - 1;
+    $endIndex = min($endIndex, $totalItems - 1);
+
+    // Get the slice of users for the current page
+    $paginatedUsers = array_slice($users, $startIndex, $itemsPerPage);
 ?>
     <table>
-    <thead>
-        <tr>
-            <th class="<?= getSortClass('id') ?>">
-                <a href="?sort=id&order=<?= getSortOrder('id') ?>">
-                    Id <?= getSortArrow('id') ?>
-                </a>
-            </th>
-            <th class="<?= getSortClass('firstname') ?>">
-                <a href="?sort=firstname&order=<?= getSortOrder('firstname') ?>">
-                    Firstname <?= getSortArrow('firstname') ?>
-                </a>
-            </th>
-            <th class="<?= getSortClass('lastname') ?>">
-                <a href="?sort=lastname&order=<?= getSortOrder('lastname') ?>">
-                    Lastname <?= getSortArrow('lastname') ?>
-                </a>
-            </th>
-            <th class="<?= getSortClass('email') ?>">
-                <a href="?sort=email&order=<?= getSortOrder('email') ?>">
-                    Email <?= getSortArrow('email') ?>
-                </a>
-            </th>
-            <th class="<?= getSortClass('role_id') ?>">
-                <a href="?sort=role_id&order=<?= getSortOrder('role_id') ?>">
-                    Role <?= getSortArrow('role_id') ?>
-                </a>
-            </th>
-            <th class="<?= getSortClass('email_verified') ?>">
-                <a href="?sort=email_verified&order=<?= getSortOrder('email_verified') ?>">
-                    Email verified <?= getSortArrow('email_verified') ?>
-                </a>
-            </th>
-            <th class="<?= getSortClass('date_inserted') ?>">
-                <a href="?sort=date_inserted&order=<?= getSortOrder('date_inserted') ?>">
-                    Date inserted <?= getSortArrow('date_inserted') ?>
-                </a>
-            </th>
-            <th class="<?= getSortClass('date_updated') ?>">
-                <a href="?sort=date_updated&order=<?= getSortOrder('date_updated') ?>">
-                    Date updated <?= getSortArrow('date_updated') ?>
-                </a>
-            </th>
-            <th>Actions</th>
-        </tr>
-    </thead>
+        <thead>
+            <tr>
+                <th class="<?= getSortClass('id') ?>">
+                    <a href="?sort=id&order=<?= getSortOrder('id') ?>">
+                        Id <?= getSortArrow('id') ?>
+                    </a>
+                </th>
+                <th class="<?= getSortClass('firstname') ?>">
+                    <a href="?sort=firstname&order=<?= getSortOrder('firstname') ?>">
+                        Firstname <?= getSortArrow('firstname') ?>
+                    </a>
+                </th>
+                <th class="<?= getSortClass('lastname') ?>">
+                    <a href="?sort=lastname&order=<?= getSortOrder('lastname') ?>">
+                        Lastname <?= getSortArrow('lastname') ?>
+                    </a>
+                </th>
+                <th class="<?= getSortClass('email') ?>">
+                    <a href="?sort=email&order=<?= getSortOrder('email') ?>">
+                        Email <?= getSortArrow('email') ?>
+                    </a>
+                </th>
+                <th class="<?= getSortClass('role_id') ?>">
+                    <a href="?sort=role_id&order=<?= getSortOrder('role_id') ?>">
+                        Role <?= getSortArrow('role_id') ?>
+                    </a>
+                </th>
+                <th class="<?= getSortClass('email_verified') ?>">
+                    <a href="?sort=email_verified&order=<?= getSortOrder('email_verified') ?>">
+                        Email verified <?= getSortArrow('email_verified') ?>
+                    </a>
+                </th>
+                <th class="<?= getSortClass('date_inserted') ?>">
+                    <a href="?sort=date_inserted&order=<?= getSortOrder('date_inserted') ?>">
+                        Date inserted <?= getSortArrow('date_inserted') ?>
+                    </a>
+                </th>
+                <th class="<?= getSortClass('date_updated') ?>">
+                    <a href="?sort=date_updated&order=<?= getSortOrder('date_updated') ?>">
+                        Date updated <?= getSortArrow('date_updated') ?>
+                    </a>
+                </th>
+                <th>Actions</th>
+            </tr>
+        </thead>
         <tbody>
             <?php
-            foreach ($users as $user) {
-            ?>
+            foreach ($paginatedUsers as $user) { ?>
+                ?>
                 <tr>
                     <td><?= $user['id'] ?></td>
                     <td><?= $user['firstname'] ?></td>
@@ -87,12 +100,16 @@ if ($user['role_id'] == 1 && isset($users)) {
             ?>
         </tbody>
     </table>
-
+    <div class="pagination">
+        <?php for ($page = 1; $page <= $totalPages; $page++) { ?>
+            <a href="?page=<?= $page ?>" <?= $page === $currentPage ? 'class="active"' : '' ?>>
+                <?= $page ?>
+            </a>
+        <?php } ?>
+    </div>
 <?php
 }
-?>
 
-<?php
 function getSortClass($column)
 {
     if (isset($_GET['sort']) && $_GET['sort'] === $column) {

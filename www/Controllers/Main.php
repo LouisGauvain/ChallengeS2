8411002eb2a416de78ff092cf57ccf82339348b1
skydogzz <thomas.stephan@live.fr>
2023-06-28 09:14:24 +0000

    if users is admin assign user management

--- www/Controllers/Main.php
+++ www/Controllers/Main.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Core\View;
 use App\Core\Utils;
+use App\Controllers\Dashboard;
 
 class Main
 {
@@ -26,6 +27,12 @@ class Main
 
         if (!isset($_SESSION['user']['id'])) {
             Utils::redirect("login");
+        }
+
+        if($_SESSION['user']['role_id'] == 1){
+            $dashboard = new Dashboard();
+
+            $view->assign("users", $dashboard->users());
         }
     }
 }

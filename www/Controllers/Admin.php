7cd34240d2523349e22da754a306e764e693c17d
skydogzz <thomas.stephan@live.fr>
2023-06-28 14:22:59 +0000

    update he user if noone else have  is new email

--- www/Controllers/Admin.php
+++ www/Controllers/Admin.php
@@ -6,13 +6,15 @@ use App\Core\Utils;
 use App\Models\Users;
 use App\Core\View;
 use App\Forms\EditUser;
+use App\Core\Verificator;
 
-class Admin{
+class Admin
+{
 
     public function deleteUser()
     {
         $user = new Users();
-        $user->delete($_GET['id']); 
+        $user->delete($_GET['id']);
         Utils::redirect('/dashboard');
     }
 
@@ -25,6 +27,51 @@ class Admin{
 
         $view->assign("form", $form->getConfig());
         $view->assign("fullname", $userInfos['firstname'] . " " . $userInfos['lastname']);
+        if ($form->isSubmit()) {
+            $errors = Verificator::formEditUser($form->getConfig(), $_POST);
+            if (empty($errors)) {
+                $user = new Users();
+                if ($user->emailExist($_POST['user_email']) && $_POST['user_email'] != $userInfos['email']) {
+                    $errors['user_email'] = "Cet email existe déjà";
+                    $view->assign('errors', $errors);
+                } else {
+                    $user->setId($userInfos['id']);
+                    $user->setEmail($_POST['user_email']);
+                    $user->setFirstname($_POST['user_firstname']);
+                    $user->setLastname($_POST['user_lastname']);
+                    switch ($_POST['user_role_id']) {
+                        case 'admin':
+                            $user->setRoleId(1);
+                            break;
+                        case 'editor':
+                            $user->setRoleId(2);
+                            break;
+                        case 'author':
+                            $user->setRoleId(3);
+                            break;
+                        case 'contributor':
+                            $user->setRoleId(4);
+                            break;
+                        case 'subscriber':
+                            $user->setRoleId(5);
+                            break;
+                        default:
+                            $user->setRoleId(6);
+                            break;
+                    }
+                    if (isset($_POST['user_email_verified']) && $_POST['user_email_verified'] == "on") {
+                        $user->setEmailVerified(1);
+                    } else {
+                        $user->setEmailVerified(0);
+                    }
+                    $user->setDateUpdated(date("Y-m-d H:i:s"));
+                    $user->setPassword($userInfos['password']);
+                    $user->save($_GET['id']);
+                    Utils::redirect('/dashboard');
+                }
+            } else {
+                $view->assign('errors', $errors);
+            }
+        }
     }
-
 }

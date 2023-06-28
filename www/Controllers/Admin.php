e90f6c9f0ef9a0dc7625c69ae54e3982987e1e36
skydogzz <thomas.stephan@live.fr>
2023-06-28 13:43:16 +0000

    render the form if not submited

--- www/Controllers/Admin.php
+++ www/Controllers/Admin.php
@@ -4,19 +4,27 @@ namespace App\Controllers;
 
 use App\Core\Utils;
 use App\Models\Users;
+use App\Core\View;
+use App\Forms\EditUser;
 
 class Admin{
 
     public function deleteUser()
     {
         $user = new Users();
-        $user->delete($_GET['id']);
+        $user->delete($_GET['id']); 
         Utils::redirect('/dashboard');
     }
 
     public function editUser()
     {
-        echo "editUser";
+        $user = new Users();
+        $userInfos = $user->find($_GET['id']);
+        $form = new EditUser($userInfos);
+        $view = new View("Admin/edit_user", "front");
+
+        $view->assign("form", $form->getConfig());
+        $view->assign("fullname", $userInfos['firstname'] . " " . $userInfos['lastname']);
     }
 
 }

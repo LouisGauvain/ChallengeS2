f611e5e85b3bdf7848424b5b9abfcec1edf5d60e
skydogzz <thomas.stephan@live.fr>
2023-06-28 09:14:35 +0000

    display users table

--- www/Views/Main/dashboard.view.php
+++ www/Views/Main/dashboard.view.php
@@ -12,3 +12,48 @@ Voici un récapitulatif de vos informations :
         echo $key . " : " . $value . "<br>";
     }
     ?>
+
+<?php
+if($user['role_id'] == 1 && isset($users)){
+    ?>
+<table>
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Email verified</th>
+            <th>Date inserted</th>
+            <th>Date updated</th>
+            <th>Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        foreach ($users as $user) {
+            ?>
+            <tr>
+                <td><?= $user['id'] ?></td>
+                <td><?= $user['firstname'] ?></td>
+                <td><?= $user['lastname'] ?></td>
+                <td><?= $user['email'] ?></td>
+                <td><?= $user['role_id'] ?></td>
+                <td><?= $user['email_verified'] ?></td>
+                <td><?= $user['date_inserted'] ?></td>
+                <td><?= $user['date_updated'] ?></td>
+                <td>
+                        <a <?php if ( $user['role_id'] === 1) echo 'class="disabled"' ?> href="edit.php?id=<?= $user['id'] ?>">Modifier</a> |
+                        <a <?php if ( $user['role_id'] === 1) echo 'class="disabled"' ?> href="delete.php?id=<?= $user['id'] ?>">Supprimer</a>
+                </td>
+            </tr>
+            <?php
+        }
+        ?>
+    </tbody>
+</table>
+
+    <?php
+}
+?>

e500ee2e12a06aaf699a21f447800c38697f0833
skydogzz <thomas.stephan@live.fr>
2023-06-28 13:39:05 +0000

    add checkbox gestion

--- www/Views/Modals/form.php
+++ www/Views/Modals/form.php
@@ -1,3 +1,8 @@
+<?php
+
+use App\Core\Utils;
+
+?>
 <form
         method="<?= $config["config"]["method"]??"GET" ?>"
         action="<?= $config["config"]["action"] ?>">
@@ -10,16 +15,26 @@
                     <option><?= $option;?></option>
                 <?php endforeach;?>
             </select>
-        <?php else: ?>
+        <?php elseif($input["type"] == "checkbox"):?>
+            <input
+                    name="<?= $name;?>"
+                    type="<?= $input["type"]?>"
+                    <?php if(isset($input["checked"]) && $input["checked"] == true):?>
+                        checked
+                    <?php endif;?>
+            >
+            <label for="<?= $name;?>"><?= $input["label"];?></label>
+        <?php else : ?>
             <input
                     name="<?= $name;?>"
                     type="<?= $input["type"]?>"
                     placeholder=" <?= $input["placeholder"]?>"
+                    value="<?= $input["value"]??"";?>"
+                    
             >
         <?php endif;?>
 
     <?php endforeach; ?>
-
 
 
     <input type="submit" name="submit" value="<?= $config["config"]["submit"] ?>">

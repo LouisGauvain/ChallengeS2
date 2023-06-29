26c8fe229b7c07d0be245af88ddf8daf964eadac
louisgauvain <92375006+LouisGauvain@users.noreply.github.com>
2023-06-29 11:30:57 +0200

    add input add template page

---
+++ www/Forms/AddTemplatePage.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Forms;
+
+use App\Forms\Abstract\AForm;
+
+class AddTemplatePage extends AForm
+{
+
+    protected $method = "POST";
+
+    public function getConfig(): array
+    {
+        return [
+            "config" => [
+                "method" => $this->getMethod(),
+                "action" => "",
+                "enctype" => "multipart/form-data",
+                "submit" => "Confirmer",
+                "cancel" => "Annuler"
+            ],
+            "inputs" => [
+                "template_name" => [
+                    "type" => "text",
+                    "placeholder" => "Le nom du template",
+                    "min" => 2,
+                    "max" => 45,
+                    "label" => "",
+                    "error" => "Le nom du template doit faire entre 2 et 45 caractères"
+                ],
+                "template_description" => [
+                    "type" => "texte",
+                    "placeholder" => "Code du template",
+                    "min" => "",
+                    "max" => "",
+                    "label" => "",
+                    "error" => "La description n'as pas pu être ajouté"
+                ],
+                "template_image" => [
+                    "type" => "file",
+                    "placeholder" => "L'image du template",
+                    "min" => "",
+                    "max" => "",
+                    "label" => "",
+                    "error" => "L'image n'as pas pu être ajouté"
+                ],
+            ],
+        ];
+    }
+}

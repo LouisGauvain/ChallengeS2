24fefec8e268b68761b52d794793e02151440a43
skydogzz <thomas.stephan@live.fr>
2023-06-30 09:38:53 +0000

    fix bug for empty placeholder

--- www/Views/Modals/form.php
+++ www/Views/Modals/form.php
@@ -15,7 +15,7 @@ use App\Core\Utils;
             <input class="<?= $input["class"] ?>" name="<?= $name; ?>" type="<?= $input["type"] ?>" <?php if (isset($input["checked"]) && $input["checked"] == true) : ?> checked <?php endif; ?>>
             <label class="<?= $input["class"] ?>" for="<?= $name; ?>"><?= $input["label"]; ?></label>
         <?php else : ?>
-            <input class="<?= $input["class"] ?>" name="<?= $name; ?>" type="<?= $input["type"] ?>" placeholder="<?= $input["placeholder"] ?>" value="<?= $input["value"] ?? ""; ?>">
+            <input class="<?= $input["class"] ?? "" ?>" name="<?= $name; ?>" type="<?= $input["type"] ?>" placeholder="<?= $input["placeholder"] ?>" value="<?= $input["value"] ?? ""; ?>">
         <?php endif; ?>
     <?php endforeach; ?>
     <input type="submit" name="submit" value="<?= $config["config"]["submit"] ?>">

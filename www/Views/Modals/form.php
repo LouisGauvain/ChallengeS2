8eee1c986d19f36142619a34a8f983d34f6b2bcc
louisgauvain <92375006+LouisGauvain@users.noreply.github.com>
2023-06-30 12:01:12 +0200

    add radio avec label

--- www/Views/Modals/form.php
+++ www/Views/Modals/form.php
@@ -6,14 +6,17 @@ use App\Core\Utils;
 <form method="<?= $config["config"]["method"] ?? "GET" ?>" action="<?= $config["config"]["action"] ?>" enctype="<?= $config["config"]["enctype"] ?>">
     <?php foreach ($config["inputs"] as $name => $input) : ?>
         <?php if ($input["type"] == "select") : ?>
-            <select class="<?= $input["class"] ?>" name="<?= $name; ?>">
+            <select class="<?= $input["class"] ?? "" ?>" name="<?= $name; ?>">
                 <?php foreach ($input["options"] as $option) : ?>
                     <option><?= $option; ?></option>
                 <?php endforeach; ?>
             </select>
         <?php elseif ($input["type"] == "checkbox") : ?>
-            <input class="<?= $input["class"] ?>" name="<?= $name; ?>" type="<?= $input["type"] ?>" <?php if (isset($input["checked"]) && $input["checked"] == true) : ?> checked <?php endif; ?>>
-            <label class="<?= $input["class"] ?>" for="<?= $name; ?>"><?= $input["label"]; ?></label>
+            <input class="<?= $input["class"] ?? "" ?>" name="<?= $name; ?>" type="<?= $input["type"] ?>" <?php if (isset($input["checked"]) && $input["checked"] == true) : ?> checked <?php endif; ?>>
+            <label class="<?= $input["class"] ?? "" ?>" for="<?= $name; ?>"><?= $input["label"]; ?></label>
+        <?php elseif ($input["type"] == "radio") : ?>
+            <input class="<?= $input["class"] ?? "" ?>" name="<?= $name; ?>" type="<?= $input["type"] ?>" <?php if (isset($input["checked"]) && $input["checked"] == true) : ?> checked <?php endif; ?>>
+            <label class="<?= $input["class"] ?? "" ?>" for="<?= $name; ?>"><?= $input["label"]; ?></label>
         <?php else : ?>
             <input class="<?= $input["class"] ?? "" ?>" name="<?= $name; ?>" type="<?= $input["type"] ?>" placeholder="<?= $input["placeholder"] ?>" value="<?= $input["value"] ?? ""; ?>">
         <?php endif; ?>

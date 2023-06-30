df3e077468ed344b9df506bd80b268e5bbd54cd4
louisgauvain <92375006+LouisGauvain@users.noreply.github.com>
2023-06-30 11:07:19 +0200

    resolve bug add template js

--- www/Views/Modals/form.php
+++ www/Views/Modals/form.php
@@ -3,40 +3,21 @@
 use App\Core\Utils;
 
 ?>
-<form
-        method="<?= $config["config"]["method"]??"GET" ?>"
-        action="<?= $config["config"]["action"] ?>">
-
-    <?php foreach ($config["inputs"] as $name=>$input):?>
-
-        <?php if($input["type"] == "select"):?>
-            <select name="<?= $name;?>">
-                <?php foreach ($input["options"] as $option):?>
-                    <option><?= $option;?></option>
-                <?php endforeach;?>
+<form method="<?= $config["config"]["method"] ?? "GET" ?>" action="<?= $config["config"]["action"] ?>" enctype="<?= $config["config"]["enctype"] ?>">
+    <?php foreach ($config["inputs"] as $name => $input) : ?>
+        <?php if ($input["type"] == "select") : ?>
+            <select class="<?= $input["class"] ?>" name="<?= $name; ?>">
+                <?php foreach ($input["options"] as $option) : ?>
+                    <option><?= $option; ?></option>
+                <?php endforeach; ?>
             </select>
-        <?php elseif($input["type"] == "checkbox"):?>
-            <input
-                    name="<?= $name;?>"
-                    type="<?= $input["type"]?>"
-                    <?php if(isset($input["checked"]) && $input["checked"] == true):?>
-                        checked
-                    <?php endif;?>
-            >
-            <label for="<?= $name;?>"><?= $input["label"];?></label>
+        <?php elseif ($input["type"] == "checkbox") : ?>
+            <input class="<?= $input["class"] ?>" name="<?= $name; ?>" type="<?= $input["type"] ?>" <?php if (isset($input["checked"]) && $input["checked"] == true) : ?> checked <?php endif; ?>>
+            <label class="<?= $input["class"] ?>" for="<?= $name; ?>"><?= $input["label"]; ?></label>
         <?php else : ?>
-            <input
-                    name="<?= $name;?>"
-                    type="<?= $input["type"]?>"
-                    placeholder=" <?= $input["placeholder"]?>"
-                    value="<?= $input["value"]??"";?>"
-                    
-            >
-        <?php endif;?>
-
+            <input class="<?= $input["class"] ?>" name="<?= $name; ?>" type="<?= $input["type"] ?>" placeholder="<?= $input["placeholder"] ?>" value="<?= $input["value"] ?? ""; ?>">
+        <?php endif; ?>
     <?php endforeach; ?>
-
-
     <input type="submit" name="submit" value="<?= $config["config"]["submit"] ?>">
     <input type="reset" value="<?= $config["config"]["cancel"] ?>">
 </form>

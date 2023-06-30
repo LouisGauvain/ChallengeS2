7fefde159ef52a85d2542cfeffd235994a80a2fe
louisgauvain <92375006+LouisGauvain@users.noreply.github.com>
2023-06-30 15:26:33 +0200

    add js only page add_template_page

--- www/public/js/js_input_create_template.js
+++ www/public/js/js_input_create_template.js
@@ -1,8 +1,14 @@
 // code pour voir en temps réel le code html dans la page de création de template
-const previewContainer = document.getElementById("preview-container");
-const codeEditor = document.getElementsByClassName("code-editor")[0];
+const pageURL = window.location.href;
+const path = pageURL.substring(pageURL.lastIndexOf("/") + 1);
 
-codeEditor.addEventListener("keyup", function () {
-    var htmlCode = codeEditor.value;
-    previewContainer.innerHTML = htmlCode;
-});
+if (path === "add_Template_Page") {
+    const previewContainer = document.getElementById("preview-container");
+    const codeEditor = document.getElementsByClassName("code-editor")[0];
+
+    codeEditor.addEventListener("keyup", function () {
+        var htmlCode = codeEditor.value;
+        previewContainer.innerHTML = htmlCode;
+    });
+}
+

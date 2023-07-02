7ed5f26f187635792356cea8fd556f5e1463f8fd
skydogzz <thomas.stephan@live.fr>
2023-07-02 17:34:04 +0000

    create navbar components

---
+++ www/Views/Main/components.view.php
@@ -0,0 +1,84 @@
+<h2 class="mt-2">Links</h2>
+<div class="flex">
+    <a href="#">Href</a>
+    <a href="#" class="disabled">Href Disabled</a>
+</div>
+
+<h2 class="mt-2">Button</h2>
+<div class="flex">
+    <div class="flex column">
+        <button class="large users dark">Utilisateurs</button>
+        <button class="large users light">Utilisateurs</button>
+    </div>
+
+    <div class="flex column">
+        <button class="large dashboard dark">Dashboard</button>
+        <button class="large dashboard light">Dashboard</button>
+    </div>
+
+    <div class="flex column">
+        <button class="large comments dark">Commentaires</button>
+        <button class="large comments light">Commentaires</button>
+    </div>
+
+    <div class="flex column">
+        <button class="large medias dark">Medias</button>
+        <button class="large medias light">Medias</button>
+    </div>
+
+    <div class="flex column">
+        <button class="large pages dark">Pages</button>
+        <button class="large pages light">Pages</button>
+    </div>
+
+    <div class="flex column">
+        <button class="large templates dark">Templates</button>
+        <button class="large templates light">Templates</button>
+    </div>
+
+    <div class="flex column">
+        <button class="large seo dark">SEO</button>
+        <button class="large seo light">SEO</button>
+    </div>
+
+    <div class="flex column">
+        <button class="large settings dark">Settings</button>
+        <button class="large settings light">Settings</button>
+    </div>
+
+    <div class="flex column">
+        <button class="large home dark">Accueil</button>
+        <button class="large home light">Accueil</button>
+    </div>
+
+    <div class="flex column">
+        <button class="large menu dark">Menu</button>
+        <button class="large menu light">Menu</button>
+    </div>
+</div>
+
+<h2 class="mt-2">Navbar</h2>
+<nav class="navbar flex" id="menu">
+    <div class="flex column no-gap align-start height-100 justify-start bg-color">
+        <button class="large home dark">Accueil</button>
+        <button class="large dashboard dark">Dashboard</button>
+        <button class="large pages dark">Pages</button>
+        <button class="large comments dark">Commentaires</button>
+        <button class="large medias dark">Medias</button>
+        <button class="large templates dark">Templates</button>
+        <button class="large users dark">Utilisateurs</button>
+        <button class="large settings dark">Settings</button>
+        <button class="large seo dark">SEO</button>
+        <button class="large menu dark mt-auto">Menu</button>
+    </div>
+</nav>
+
+<script>
+    const menu = document.querySelector('nav .menu');
+    const navbar = document.querySelector('.navbar');
+
+    console.log(menu);
+    menu.addEventListener('click', () => {
+        navbar.classList.toggle('wrapped');
+    });
+</script>

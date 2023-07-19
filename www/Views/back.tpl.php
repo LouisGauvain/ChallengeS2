605178ae4548c667c8498db0a04989f406c87dc3
skydogzz <thomas.stephan@live.fr>
2023-07-19 16:01:12 +0000

    create header if connected

--- www/Views/back.tpl.php
+++ www/Views/back.tpl.php
@@ -13,10 +13,33 @@
     <!-- <script src="tinymce/js/tinymce/tinymce.min.js"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-</head>
+    <style>
+        header {
+            background-color: lightgrey;
+            padding: 10px;
+            margin-bottom: 20px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        header a {
+            margin-left: 10px;
+        }
+    </style>
+    </head>
 
 <body class="backend">
     <div id="root">
+        <?php
+    if (!empty($_SESSION['user'])) {
+        ?><header>
+        <a href="/" class="btn btn-primary">Voir le site</a>
+        <a href="/disconnect" class="btn btn-danger">Déconnexion</a>
+    </header>
+        <?php
+    }
+    ?>
         <?php include __DIR__ . '/Components/navbar.view.php'; ?>
         
         <?php include $this->view; ?>
--- www/Views/front.tpl.php
+++ www/Views/front.tpl.php
@@ -9,12 +9,33 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
+<style>
+        header {
+            background-color: lightgrey;
+            padding: 10px;
+            margin-bottom: 20px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
 
-<body>
+        header a {
+            margin-left: 10px;
+        }
+    </style>
+<body class="frontend">
     <div id="root">
+        <?php
+    if (!empty($_SESSION['user'])) {
+        ?><header>
+        <a href="/dashboard" class="btn btn-primary">Dashboard</a>
+        <a href="/disconnect" class="btn btn-danger">Déconnexion</a>
+    </header>
+        <?php
+    }
+    ?>
+
         <?php include $this->view; ?>
-        
-        <a href="disconnect?controller=security&task=logout">Déconnexion</a>
     </div>
 </body>
 

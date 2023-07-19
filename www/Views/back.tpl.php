30d3dbc45d91b4d8a70a9ca70042f3d5727087b0
skydogzz <thomas.stephan@live.fr>
2023-07-19 15:28:21 +0000

    add bootstrap

--- www/Views/back.tpl.php
+++ www/Views/back.tpl.php
@@ -11,12 +11,13 @@
     <script src="public/js/js_input_file.js" defer></script>
     <!-- <script src="public/js/js_input_texte.js" defer></script> -->
     <!-- <script src="tinymce/js/tinymce/tinymce.min.js"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 
 <body class="backend">
     <?php include __DIR__ . '/Components/navbar.view.php'; ?>
-    <h1>Template de back</h1>
-
+    
     <?php include $this->view; ?>
 
     <a href="disconnect?controller=security&task=logout">Déconnexion</a>

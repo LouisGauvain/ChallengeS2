bbe578158679daed6b0f897cf4c8f3d632108e44
louisgauvain <92375006+LouisGauvain@users.noreply.github.com>
2023-07-20 10:39:38 +0200

    add style scss

--- www/Views/back.tpl.php
+++ www/Views/back.tpl.php
@@ -4,72 +4,39 @@
 <head>
     <meta charset="UTF-8">
     <title><?= $this->data['page_title'] ?></title>
+    <link rel="stylesheet" href="public/css/style.css">
     <meta name="description" content="Ceci est ma super page">
     <script src="public/js/js_input_create_template.js" defer></script>
     <script src="public/js/js_input_radio_only_one.js" defer></script>
     <script src="public/js/js_input_file.js" defer></script>
-    <!-- <script src="public/js/js_input_texte.js" defer></script> -->
-    <!-- <script src="tinymce/js/tinymce/tinymce.min.js"></script> -->
+    <script src="public/js/js_input_texte.js" defer></script>
+    <script src="tinymce/js/tinymce/tinymce.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <style>
-        *, *::before, *::after {
-            box-sizing: border-box;
-            margin: 0;
-            padding: 0;
-        }
-        header {
-            background-color: lightgrey;
-            padding: 10px;
-            margin-bottom: 20px;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-        }
-
-        header a {
-            margin-left: 10px;
-        }
-
-        .sidebar {
-            background-color: lightgrey;
-            width: 250px;
-            height: 100vh;
-            position: fixed;
-            top: 0;
-            left: 0;
-            padding: 20px;
-        }
-
-        .content {
-            margin-left: 250px;
-            /* Largeur de la barre de navigation */
-        }
-    </style>
 </head>
 
 <body class="backend">
     <div id="root">
-    <?php
-            if (!empty($_SESSION['user'])) {
-            ?><div class="sidebar">
-            <ul class="nav flex-column">
-                <li class="nav-item">
-                    <a class="nav-link" href="/dashboard">Dashboard</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/profile">Profile</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/settings">Settings</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/logout">Logout</a>
-                </li>
-            </ul>
-        </div> <?php
-            }
-            ?>
+        <?php
+        if (!empty($_SESSION['user'])) {
+        ?><div class="sidebar">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/dashboard">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/profile">Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/settings">Settings</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/logout">Logout</a>
+                    </li>
+                </ul>
+            </div> <?php
+                }
+                    ?>
         <div class="content">
             <?php
             if (!empty($_SESSION['user'])) {
@@ -80,10 +47,9 @@
             <?php
             }
             ?>
-
-            <?php include $this->view; ?>
-
-            <a href="disconnect?controller=security&task=logout">Déconnexion</a>
+            <div class="info_user">
+                <?php include $this->view; ?>
+            </div>
         </div>
     </div>
 </body>

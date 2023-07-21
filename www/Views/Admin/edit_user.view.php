785648eaf5b680d43819b9bbd8a086bafc92f0a8
louisgauvain <92375006+LouisGauvain@users.noreply.github.com>
2023-07-21 06:15:37 +0200

    ded

--- www/Views/Admin/edit_user.view.php
+++ www/Views/Admin/edit_user.view.php
@@ -1,5 +1,7 @@
-<h2>Modifier user: <?php echo $fullname ?></h2>
+<div class="div_input">
+    <h2>Modifier user: <?php echo $fullname ?></h2>
 
-<?php print_r($errors ?? null); ?>
+    <?php print_r($errors ?? null); ?>
 
-<?php $this->modal("form", $form); ?> 
+    <?php $this->modal("form", $form); ?>
+</div>

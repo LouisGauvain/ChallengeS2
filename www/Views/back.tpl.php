8bdb817378e3f187bf770b68928e8d9f099b84f0
louisgauvain <92375006+LouisGauvain@users.noreply.github.com>
2023-07-21 02:54:07 +0200

    aa

--- www/Views/back.tpl.php
+++ www/Views/back.tpl.php
@@ -6,9 +6,6 @@
     <title><?= $this->data['page_title'] ?></title>
     <link rel="stylesheet" href="public/css/style.css">
     <meta name="description" content="Ceci est ma super page">
-    <script src="public/js/js_input_create_template.js" defer></script>
-    <script src="public/js/js_input_radio_only_one.js" defer></script>
-    <script src="public/js/js_input_file.js" defer></script>
     <script src="/public/js/htmlToJson.js"></script>
     <script src="public/js/page.js" defer></script>
     <script src="tinymce/js/tinymce/tinymce.min.js"></script>

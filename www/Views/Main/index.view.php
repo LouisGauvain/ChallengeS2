6980ba104f3351adce318ffdd10bca48f57d3963
skydogzz <thomas.stephan@live.fr>
2023-07-21 02:12:07 +0000

    manage pages

--- www/Views/Main/index.view.php
+++ www/Views/Main/index.view.php
@@ -1,10 +1,34 @@
+<?php $user = $_SESSION['user']; ?>
 <div class="container">
-    <h1>Liste des Pages</h1>
-    <ul class="list-group">
-        <?php 
-        foreach($pages as $page){
-            echo "<li class='list-group-item'><a href='".$page['url_page']."'>".$page['title']."</a></li>";
-        }
-        ?>
-    </ul>
+    <h2>Vos Pages</h2>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Titre</th>
+                <th>URL</th>
+                <th>Template</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($pages as $page) {
+                echo "<tr>";
+                echo "<td><a href='" . $page['url_page'] . "'>" . $page['title'] . "</a></td>";
+                echo "<td>" . $page['url_page'] . "</td>";
+                echo "<td>" . $page['used_template'] . "</td>";
+                ?>
+                <td>
+                    <?php if ($user['role_id'] == 1) { ?>
+                        <a href="<?= $page['url_page'] ?>">Voir</a> |
+                        <a href="admin/delete_page?id=<?= $page['id'] ?>" onclick="return confirm('Êtes-vous sûr de vouloir supprimer cet page ?')">Supprimer</a>
+                    <?php } ?>
+                </td>
+                <?php
+                echo "</tr>";    
+
+             }
+            ?>
+        </tbody>
+    </table>
 </div>

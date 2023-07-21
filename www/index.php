bf4a889747ff7b650aa9d0cfdadf0caee5ef9670
louisgauvain <92375006+LouisGauvain@users.noreply.github.com>
2023-07-21 05:53:58 +0200

    commentaire

--- www/index.php
+++ www/index.php
@@ -51,35 +51,11 @@ if(!empty($missingkeys)) {
   Utils::redirect("install.php");
 }
 
-//Récupérer dans l'url l'uri /login ou /user/toto
-//Nettoyer la donnée
 //S'il y a des paramètres dans l'url il faut les enlever :
 $uriExploded = explode("?", $_SERVER["REQUEST_URI"]);
 $uri = rtrim(strtolower(trim($uriExploded[0])), "/");
 //Dans le cas ou nous sommes à la racine $uri sera vide du coup je remets /
 $uri = (empty($uri)) ? "/" : $uri;
-
-//Créer un fichier yaml contenant le route du type :
-// /login:
-//      controller: Security
-//      action: login
-//Le fichier est dans www et porte le nom de routes.yml
-
-//En fonction de l'uri récupérer le controller et l'action
-//Effectuer toutes les vérifications que vous estimez nécessaires
-//En cas d'erreur effectuer un simple Die avec un message explicite
-//Dans le cas ou tout est bon créer une instance du controller
-//et appeler la methode action
-
-//Exemple :
-// $controller = new Security();
-// $controller->login();
-
-// if (!file_exists("routes.yml")) {
-//     die("Le fichier de routing n'existe pas");
-// }
-
-// $routes = yaml_parse_file("routes.yml");
 
 $found = false;
 $pages = new Pages();
@@ -113,91 +89,3 @@ if (!$found) {
 
 $sitemapController = new Sitemap();
 $sitemapController->generateSitemap();
-
-
-
-// if (empty($routes[$uri])) {
-//     header("HTTP/1.0 404 Not Found");
-//     die("Page 404");
-// }
-
-// if (empty($controller) || empty($action)) {
-//     die("Absence de controller ou d'action dans le ficher de routing pour la route " . $uri);
-// }
-
-// $controller = $routes[$uri]["controller"];
-// $action = $routes[$uri]["action"];
-
-//Vérification de l'existance de la classe
-// if (!file_exists("Controllers/" . $controller . ".php")) {
-//     die("Le fichier Controllers/" . $controller . ".php n'existe pas");
-// }
-
-// include "Controllers/" . $controller . ".php";
-
-//Le fichier existe mais est-ce qu'il possède la bonne class    e
-//bien penser à ajouter le namespace \App\Controllers\Security
-// $controller = "\\App\\Controllers\\" . $controller;
-// if (!class_exists($controller)) {
-//     die("La class " . $controller . " n'existe pas");
-// }
-
-// $objet = new $controller();
-
-// //Est-ce que l'objet contient bien la methode
-// if (!method_exists($objet, $action)) {
-//     die("L'action " . $action . " n'existe pas");
-// }
-
-// $objet->$action();
-
-?>
-
-<!-- <div id="test"></div>
-<script>
-  url = "<?php /*echo $_ENV['API_URL']*/ ?>"
-
-  json = {
-    type: "table",
-    children: [{
-      type: "tbody",
-      children: Array.from({
-        length: 5
-      }, function(_, i) {
-        return {
-          type: "tr",
-          children: Array.from({
-            length: 5
-          }, function(_, j) {
-            return {
-              type: "td",
-              children: ["Cell " + i + "-" + j, ],
-            };
-          }),
-        };
-      }),
-    }, ],
-  }
-
-  //test if json is valid
-  if (typeof json !== "object") {
-    throw new Error("Invalid JSON");
-  }
-
-  //call api send json
-  fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(json)
-    })
-    .then(response => response.text())
-    .then(htmlResponse => {
-      document.getElementById("test").innerHTML = htmlResponse;
-    })
-    .catch(error => {
-      console.error('Une erreur s\'est produite:', error);
-    });
-
-</script> -->

544790281d1cc8f9b10f2786f259ea95a7dfc9c7
skydogzz <thomas.stephan@live.fr>
2023-09-17 07:32:59 +0000

    customize error message

--- www/install/test.php
+++ www/install/test.php
@@ -11,6 +11,20 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo json_encode(['success' => true]);
 } catch (PDOException $e) {
-    echo json_encode(['success' => false, 'message' => $e->getMessage()]);
+    $errorCode = $e->getCode();
+    $errorMessage = $e->getMessage();
+
+    // Traduire ou personnaliser le message d'erreur en fonction de son code ou de son contenu
+    switch ($errorCode) {
+        case 1045:
+            echo json_encode(["message"=>"Nom d'utilisateur ou mot de passe incorrect."]);
+            break;
+        case 2002:
+            echo json_encode(["message"=> "Impossible de se connecter au serveur de base de données."]);
+            break;
+        default:
+        echo json_encode(["message"=> "Une erreur est survenue lors de la connexion à la base de données."]);
+            break;
+    }
 }
 

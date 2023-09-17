ddb6a9a846a37499bfaa797b66de28c011307128
skydogzz <thomas.stephan@live.fr>
2023-09-17 12:00:23 +0000

    form to setup finished

--- www/public/js/framework/src/components/Install.js
+++ www/public/js/framework/src/components/Install.js
@@ -3,14 +3,6 @@ import Input from "./Input.js"
 import Button from "./Button.js";
 
 export default function Install({ step = 1, errors, verified }) {
-    //installeur style temporaire
-    const installerStyle = {
-        backgroundColor: "grey",
-        borderRadius: "5px",
-        width: "100vw",
-        height: "50vh"
-    };
-
     let verifyDatabaseConnetion = (e) => {
         e.preventDefault()
         let inputs = document.querySelectorAll("form .input");
@@ -51,6 +43,44 @@ export default function Install({ step = 1, errors, verified }) {
 
     let setupSite = (e) => {
         e.preventDefault()
+        let inputs = document.querySelectorAll("form .input");
+
+        let formData = {}
+        let errors = []
+
+        inputs.forEach(input => {
+            formData[input.querySelector("input").name] = input.querySelector("input").value
+            if (!input.querySelector("input").value)
+                errors.push(input.querySelector("label").textContent + " ne peut pas être vide")
+        });
+
+        if (errors.length != 0)
+            return render(Install({ step: 2, errors: errors }), document.getElementById("root2"))
+
+        const prefix = inputs[1].querySelector("input").value
+        if (prefix.length <= 2 || prefix.length > 10 || prefix[prefix.length - 1] !== "_")
+            errors.push("Le prefix doit faire entre 3 et 10 caracteres et doit se finir par un \"_\"")
+
+        const email = inputs[2].querySelector("input").value
+        const regexMail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+        if (!regexMail.test(email))
+            errors.push("Le mail renseigné ne respecte pas le format")
+
+        const password = inputs[3].querySelector("input").value
+        const passwordVerif = inputs[4].querySelector("input").value
+        const regexPasssword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/;
+
+        if (password !== passwordVerif)
+            errors.push("Les mots de passe ne corresponde pas")
+        if(!regexPasssword.test(password))
+            errors.push("Le mot de passe doit contenir, au moins une minuscule, au moins une majuscule, aumoins, un chiffre, au moins un caractere spécial, et faire au moins 8 caracteres")
+
+        if (errors.length != 0)
+            return render(Install({ step: 2, errors: errors }), document.getElementById("root2"))
+
+        sessionStorage.setItem('storedSetupFormData', JSON.stringify(formData))
+
+        console.log(inputs)
     }
 
     let children = []
@@ -99,7 +129,6 @@ export default function Install({ step = 1, errors, verified }) {
                         id: "dbPassword",
                         name: "dbPassword",
                         type: "password",
-                        ...(storedDatabaseFormData ? { value: storedDatabaseFormData.dbPassword } : {})
                     }),
                     Button({
                         title: "Vérifier la configuration",
@@ -121,6 +150,8 @@ export default function Install({ step = 1, errors, verified }) {
             }
         ]
     } else if (step == 2) {
+        let storedSetupFormData = JSON.parse(sessionStorage.getItem('storedSetupFormData') || '{}');
+
         children = [
             ...(errors ? [{
                 type: "div",
@@ -140,10 +171,37 @@ export default function Install({ step = 1, errors, verified }) {
             {
                 type: "form",
                 children: [
+                    Input({
+                        label: "Nom du site",
+                        id: "siteName",
+                        name: "siteName",
+                        ...(storedSetupFormData ? { value: storedSetupFormData.siteName } : {})
+                    }), Input({
+                        label: "Prefix des tables",
+                        id: "tablePrefix",
+                        name: "tablePrefix",
+                        ...(storedSetupFormData ? { value: storedSetupFormData.tablePrefix } : {})
+                    }), Input({
+                        label: "Email de l'Admin",
+                        id: "adminEmail",
+                        name: "adminEmail",
+                        type: "email",
+                        ...(storedSetupFormData ? { value: storedSetupFormData.adminEmail } : {}),
+                    }), Input({
+                        label: "Mot de passz de l'Admin",
+                        id: "adminPassword",
+                        name: "adminPassword",
+                        type: "password",
+                    }), Input({
+                        label: "Remettre mot de passe",
+                        id: "adminPasswordVerif",
+                        name: "adminPasswordVerif",
+                        type: "password",
+                    }),
                     Button({
                         title: "Setup le site",
                         style: {
-                            background: "aliceblue"
+                            background: "lightblue"
                         },
                         ...(verified ? { disabled: true } : {}),
                         onClick: setupSite
@@ -157,7 +215,6 @@ export default function Install({ step = 1, errors, verified }) {
         type: "div",
         attributes: {
             class: "Installer",
-            style: { ...installerStyle }
         },
         children: [
             ...children

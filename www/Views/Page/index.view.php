313ba1ca3b8fce451989480e09bf776db055bcfc
skydogzz <thomas.stephan@live.fr>
2023-09-20 03:27:13 +0000

    remove test counter

--- www/Views/Page/index.view.php
+++ www/Views/Page/index.view.php
@@ -1,13 +1,11 @@
 <div id="header"></div>
 <div id="root"></div>
-<div id="root2"></div>
 
 <script type="module">
     import { generateStructure, render } from "/public/js/framework/src/index.js"
-    import Compteur from "/public/js/framework/src/components/Compteur.js"
     import { BrowserLink, BrowserRouter } from "/public/js/framework/src/components/BrowserRouter.js"
 
-    render(<?= $page["content"] ?>, document.getElementById("root2"));
+    render(<?= $page["content"] ?>, document.getElementById("root"));
      render(BrowserRouter
         ([
             <?php
@@ -19,6 +17,4 @@
         document.getElementById("header")),
         document.getElementById("header")
     ) 
-    
-    render(Compteur({count: 0}), document.getElementById("root"));
 </script>
--- www/install.php
+++ www/install.php
@@ -14,21 +14,15 @@ if(session_status() == PHP_SESSION_NONE) {
 
 <div id="header"></div>
 <div id="root"></div>
-<div id="root2"></div>
 
 <script type="module">
     import {
         generateStructure,
         render
     } from "/public/js/framework/src/index.js"
-    import Compteur from "/public/js/framework/src/components/Compteur.js"
     import Install from "/public/js/framework/src/components/Install.js"
-
-    render(Compteur({
-        count: 0
-    }), document.getElementById("root"));
 
     render(Install({
         step: 1
-    }), document.getElementById("root2"))
+    }), document.getElementById("root"))
     </script>

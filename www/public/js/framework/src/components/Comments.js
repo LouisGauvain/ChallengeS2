b1c393089ef8c312b11aee68d9552ea772d712a9
skydogzz <thomas.stephan@live.fr>
2023-09-21 06:39:02 +0000

    add a bit of style and dont display if empty

--- www/public/js/framework/src/components/Comments.js
+++ www/public/js/framework/src/components/Comments.js
@@ -2,16 +2,53 @@ export default function Comments(comments) {
 
     if (typeof comments === "string")
         comments = JSON.parse(comments);
-    else{
+    else {
         comments = comments.children[0]
+    }
 
+    if (comments.length === 0 || comments.children.length === 0) {
+        return {
+            type: "div",
+            children: [
+                {
+                    type: "div",
+                    attributes: {
+                        style: {
+                            height: "5px",
+                            width: "80%",
+                            backgroundColor: "black",
+                            margin: "40px auto",
+                            borderRadius: "5px",
+                        },
+                    },
+                },
+                {
+                    type: "h2",
+                    children: ["Commentaires"],
+                    attributes: {
+                        style: {
+                            textAlign: "center"
+                        }
+                    }
+                },
+                {
+                    type: "p",
+                    children: ["Aucun commentaire pour le moment"],
+                    attributes: {
+                        style: {
+                            textAlign: "center"
+                        }
+                    }
+                }
+            ]
+        }
     }
     return {
         type: "div",
         children: [
             {
-                type: "h1",
-                children: ["Comments"],
+                type: "h2",
+                children: ["Commentaires"],
             },
             {
                 type: "table",
